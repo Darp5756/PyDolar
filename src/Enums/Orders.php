f8d8d19b17b9c3a2a22e7cc832eb8940c16a40c5
Darp5756 <darp7512@gmail.com>
2025-03-28 22:51:22 -0400

    Enums Orders y RoundedPrices

---
+++ src/Enums/Orders.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Darp5756\PyDolar\Enums;
+
+enum Orders: string {
+    case asc = 'asc';
+    case desc = 'desc';
+}
---
+++ src/Enums/RoundedPrices.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Darp5756\PyDolar\Enums;
+
+enum RoundedPrices: string {
+    case true = 'true';
+    case false = 'false';
+}
